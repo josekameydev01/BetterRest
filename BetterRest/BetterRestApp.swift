6a79545aa6aba99798fcaa0a99eef000c0ca930a
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-30 21:17:19 -0600

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by carlosgalvankamey on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
