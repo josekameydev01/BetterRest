3a59ea5b57112fe2ea2f5d72ba44ed61819748f6
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-02 12:38:59 -0600

    UI interface v1 - completed

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let currentDate = Date.now
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeCups = 1
+    
     
     var body: some View {
-        Text(currentDate, format: .dateTime.hour().minute())
-        Text(currentDate, format: .dateTime.day().month().year())
-        Text(currentDate.formatted(date: .numeric, time: .shortened))
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amout of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeCups) \(cupLabel(for: coffeeCups))", value: $coffeeCups, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+    }
+    private func cupLabel(for quantity: Int) -> String {
+        quantity == 1 ? "cup" : "cups"
+    }
+    private func calculateBedTime() {
+        // TODO
     }
 }
 
